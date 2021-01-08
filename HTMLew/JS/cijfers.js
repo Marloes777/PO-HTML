3ebe5f270fed468d7de3015292d1a046a610acbc
Marloes777 <77146457+Marloes777@users.noreply.github.com>
2021-01-08 10:03:37 +0100

    Add files via upload

---
+++ HTMLew/JS/cijfers.js
@@ -0,0 +1,10 @@
+
+var grades = [cijfer1, cijfer2, cijfer3, cijfer4]
+
+var total = 0;
+for(var i = 0; i < grades.length; i++) {
+    total += grades[i];
+}
+var avg = total / grades.length;
+
+Bereken.onClick; document.write; avg
